3a53c10ab9f43262057cc038bb56867b3887ecb9
iovi <ivosintsev@gmail.com>
2017-12-23 19:57:10 +0500

    ImageHelper

--- src/main/java/iovi/Captcha.java
+++ src/main/java/iovi/Captcha.java
@@ -16,7 +16,10 @@ public class Captcha {
     public Captcha(int id, int textLength){
         this.id=id;
         text=GetRandomWord(textLength);
-        image=CreateCaptchaImageFromString(text);
+
+        Font font = new Font("Arial", Font.PLAIN, 48);
+        BufferedImage textImage=ImageHelper.CreateImageFromString(text,font);
+        image=ImageHelper.CreateDistortedImage(textImage);
     }
 
     private static String GetRandomWord(int textLength){
@@ -38,70 +41,6 @@ public class Captcha {
             }
         }
         return new String(word);
-    }
-
-    BufferedImage CreateCaptchaImageFromString(String string){
-        Font font = new Font("Arial", Font.PLAIN, 48);
-        BufferedImage textImage=CreateImageFromString(string,font);
-        BufferedImage captchaImage=CreateDistortedImage(textImage);
-
-        Graphics2D captchaGrapphics = captchaImage.createGraphics();
-        captchaGrapphics.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-        captchaGrapphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        captchaGrapphics.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-        captchaGrapphics.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
-        captchaGrapphics.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-        captchaGrapphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        captchaGrapphics.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-        captchaGrapphics.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
-        captchaGrapphics.dispose();
-        return captchaImage;
-    }
-
-    BufferedImage CreateDistortedImage(BufferedImage originalImage){
-        BufferedImage distortedImage=new BufferedImage(originalImage.getWidth(), originalImage.getHeight(),
-                BufferedImage.TYPE_INT_ARGB);
-        Random random=new Random();
-        double w1=random.nextDouble()*0.1+0.05;
-        double w2=random.nextDouble()*0.1+0.05;
-        double t1=random.nextDouble()*3.14;
-        double t2=random.nextDouble()*3.14;
-        double a1=random.nextDouble()*2+5;
-        double a2=random.nextDouble()*2+5;
-
-        for(int distortedX = 0; distortedX < originalImage.getWidth(); distortedX++) {
-
-            for (int distortedY = 0; distortedY < originalImage.getHeight(); distortedY++) {
-                int originalY = distortedY + (int) (a1 * sin(w1 * distortedX + t1));
-                int originalX = distortedX + (int) (a2 * sin(w2 * distortedY + t2));
-
-                if (originalX < 0 || originalY < 0 || originalX >= originalImage.getWidth() - 1 || originalY >= originalImage.getHeight() - 1) {
-                    distortedImage.setRGB(distortedX, distortedY, 1677725);
-                } else {
-                    distortedImage.setRGB(distortedX, distortedY, originalImage.getRGB(originalX, originalY));
-                }
-            }
-        }
-        return distortedImage;
-    }
-
-    BufferedImage CreateImageFromString(String string, Font font){
-        BufferedImage bufferedImage = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D textGrapphics = bufferedImage.createGraphics();
-
-        textGrapphics.setFont(font);
-        FontMetrics fm = textGrapphics.getFontMetrics();
-        int width = fm.stringWidth(string);
-        int height = fm.getHeight();
-        textGrapphics.dispose();
-
-        bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        textGrapphics = bufferedImage.createGraphics();
-        textGrapphics.setColor(Color.BLACK);
-        textGrapphics.setFont(font);
-        textGrapphics.drawString(string, 0, fm.getAscent());
-        textGrapphics.dispose();
-        return bufferedImage;
     }
 
 }
---
+++ src/main/java/iovi/ImageHelper.java
@@ -0,0 +1,75 @@
+package iovi;
+
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+import static java.lang.Math.sin;
+
+/**
+ * Помощник при работе с изображениями
+ */
+public class ImageHelper {
+
+    /**
+     * Создает новое искаженное изображение BufferedImage из оригинального.
+     * Пикселю нового изображения с координатами (xD,yD) устанавливается
+     * цвет пикселя оригинального изображения с координатами (x0, y0).
+     * Координаты вычисляются по формулам:<br>
+     *  y0=a1*sin(w1*x+t1)<br>
+     *  x0=a2*sin(w2*y+t2)<br>
+     * где a1,a2,w1,w2,t1,t2 - случайные коэффициенты:
+     * a1 ∈ [5;7], a2 ∈ [5;7], w1 ∈ [0.05;0.15] w2 ∈ [0.05;0.15], t1 ∈ [0;3.14], t2 ∈ [0;3.14]
+     */
+
+    static BufferedImage CreateDistortedImage(BufferedImage originalImage){
+        BufferedImage distortedImage=new BufferedImage(originalImage.getWidth(), originalImage.getHeight(),
+                BufferedImage.TYPE_INT_ARGB);
+        Random random=new Random();
+        double w1=random.nextDouble()*0.1+0.05;
+        double w2=random.nextDouble()*0.1+0.05;
+        double t1=random.nextDouble()*3.14;
+        double t2=random.nextDouble()*3.14;
+        double a1=random.nextDouble()*2+5;
+        double a2=random.nextDouble()*2+5;
+
+        for(int xD = 0; xD < originalImage.getWidth(); xD++) {
+
+            for (int yD = 0; yD < originalImage.getHeight(); yD++) {
+                int y0 = yD + (int) (a1 * sin(w1 * xD + t1));
+                int x0 = xD + (int) (a2 * sin(w2 * yD + t2));
+
+                if (x0 < 0 || y0 < 0 || x0 >= originalImage.getWidth() - 1 || y0 >= originalImage.getHeight() - 1) {
+                    distortedImage.setRGB(xD, yD, 0);
+                } else {
+                    distortedImage.setRGB(xD, yD, originalImage.getRGB(x0, y0));
+                }
+            }
+        }
+        return distortedImage;
+    }
+
+    /**
+     * Создает изображение, в котором выведен указанный текст в указанном шрифте.
+     * Фон белый, цвет текста черный.
+     */
+    static BufferedImage CreateImageFromString(String string, Font font){
+        BufferedImage bufferedImage = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D textGraphics = bufferedImage.createGraphics();
+
+        textGraphics.setFont(font);
+        FontMetrics fm = textGraphics.getFontMetrics();
+        int width = fm.stringWidth(string);
+        int height = fm.getHeight();
+        textGraphics.dispose();
+
+        bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        textGraphics = bufferedImage.createGraphics();
+        textGraphics.setColor(Color.BLACK);
+        textGraphics.setFont(font);
+        textGraphics.drawString(string, 0, fm.getAscent());
+        textGraphics.dispose();
+        return bufferedImage;
+    }
+}
